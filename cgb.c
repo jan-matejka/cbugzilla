09f1653a99805042173308dc51ffec18b5e377e9
Jan (yac) Matějka <JMatejka@suse.cz>
2013-05-23 18:46:17 +0200

    cleanup

--- cgb.c
+++ cgb.c
@@ -11,47 +11,174 @@
 char *url_login = "https://bugs.gentoo.org/index.cgi";
 char *url_search_list = "https://bugs.gentoo.org/";
 char *auth_file = "./auth";
-FILE *devnull;
+char *url_namedcmd = "https://bugs.gentoo.org/buglist.cgi?cmdtype=runnamed&namedcmd=%s";
 
-struct MemoryStruct {
-    char *memory;
+// {{{ CGBString
+struct CBGString_s;
+typedef struct CGBString_s CGBString_t;
+struct CGBString_s {
+    char *mem;
     size_t size;
+    int len;
 };
 
-int gb_login(CURL *curl, char *user, char *pass)
-{
+int CGBString_init(CGBString_t cgbs) {
+  cgbs.mem = NULL;
+  cgbs.size = 0;
+  cgbs.len  = 0;
+}
+
+
+int CGBString_realloc(CGBString_t *cgbs, int len) {
+  cgbs->len  = len+1;
+  cgbs->size = sizeof(char) * (cgbs->len);
+  cgbs->mem = realloc(cgbs->mem, cgbs->size);
+  if(cgbs->mem == NULL) {
+    perror("realloc");
+    return EXIT_FAILURE;
+  }
+  cgbs->mem[len] = 0;
+}
+
+// }}}
+
+
+// {{{ CGB
+struct CGB_s;
+typedef struct CGB_s CGB_t;
+struct CGB_s {
+  CURL *curl;
   CURLcode res;
+  CGBString_t hostname;
+  CGBString_t response;
+  FILE *devnull, *log_response;
+  CGBString_t auth_user;
+  CGBString_t auth_pass;
+};
+
+CGB_t *CGB_new(void) {
+  CGB_t *cgb = malloc(sizeof(CGB_t));
+  return cgb;
+}
+
+int CGB_init(CGB_t *cgb) {
+  if(CGB_init_curl(cgb))
+    return EXIT_FAILURE;
+
+  cgb->devnull = fopen("/dev/null", "w");
+
+  CGBString_init(cgb->response);
+  CGBString_init(cgb->hostname);
+  CGBString_init(cgb->auth_user);
+  CGBString_init(cgb->auth_pass);
+
+  char hostname[] = "https://bugs.gentoo.org";
+  int hostlen = strlen(hostname);
+  CGBString_realloc(&cgb->hostname, hostlen);
+  strncpy(cgb->hostname.mem, hostname, hostlen);
+
+  CGB_authRead(cgb);
+}
+
+size_t
+CGB_curl_WMemCallback(void *ptr, size_t size, size_t nmemb, void *data)
+{
+  size_t realsize = size * nmemb;
+  size_t oldsize;
+
+  CGBString_t *cgbs = (CGBString_t *)data;
+  CGBString_init(*cgbs);
+  if(size != sizeof(char)) {
+    fprintf(stderr, "unexpected size");
+    return EXIT_FAILURE;
+  }
+  oldsize = cgbs->size;
+  CGBString_realloc(cgbs, cgbs->len + nmemb);
+
+  memcpy(&(cgbs->mem[oldsize]), ptr, realsize);
+  return realsize;
+}
+
+int CGB_init_curl(CGB_t *cgb) {
+  //char *cookiejar = strcat(getenv("HOME"), "/gb_cookiejar");
+  curl_global_init(CURL_GLOBAL_SSL);
+
+  cgb->curl = curl_easy_init();
+  if(!cgb->curl)
+    return EXIT_FAILURE;
+
+  curl_easy_setopt(cgb->curl, CURLOPT_SSL_VERIFYPEER, SKIP_PEER_VERIFICATION);
+  curl_easy_setopt(cgb->curl, CURLOPT_SSL_VERIFYHOST, SKIP_HOSTNAME_VERIFICATION);
+
+  curl_easy_setopt(cgb->curl, CURLOPT_VERBOSE, 0);
+  // curl_easy_setopt(cgb->curl, CURLOPT_COOKIEJAR, cookiejar);
+  //curl_easy_setopt(cgb->curl, CURLOPT_HEADER, 1);
+
+  curl_easy_setopt(cgb->curl, CURLOPT_WRITEFUNCTION, CGB_curl_WMemCallback);
+  curl_easy_setopt(cgb->curl, CURLOPT_WRITEDATA, (void *)&cgb->response);
+  return EXIT_SUCCESS;
+}
+
+int CGB_cleanup(CGB_t *cgb) {
+  curl_easy_cleanup(cgb->curl);
+  curl_global_cleanup();
+}
+
+int CGB_bz_login(CGB_t * cgb)
+{
   struct curl_httppost *formpost=NULL;
   struct curl_httppost *lastptr=NULL;
   struct curl_slist *headerlist=NULL;
 
-  curl_easy_setopt(curl, CURLOPT_URL, url_login);
+  curl_easy_setopt(cgb->curl, CURLOPT_URL, url_login);
 
-  /* Fill in the filename field */
   curl_formadd(&formpost,
                &lastptr,
                CURLFORM_COPYNAME, "Bugzilla_login",
-               CURLFORM_COPYCONTENTS, user,
+               CURLFORM_COPYCONTENTS, cgb->auth_user.mem,
                CURLFORM_END);
 
   curl_formadd(&formpost,
                &lastptr,
                CURLFORM_COPYNAME, "Bugzilla_password",
-               CURLFORM_COPYCONTENTS, pass,
+               CURLFORM_COPYCONTENTS, cgb->auth_pass.mem,
                CURLFORM_END);
 
-  //curl_easy_setopt(curl, CURLOPT_HEADER, 1);
-  curl_easy_setopt(curl, CURLOPT_HTTPPOST, formpost);
+  curl_easy_setopt(cgb->curl, CURLOPT_HTTPPOST, formpost);
 
-  curl_easy_setopt(curl, CURLOPT_WRITEDATA, devnull);
-  res = curl_easy_perform(curl);
-  return 1;
-   if(res != CURLE_OK)
-      fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(res));
+  cgb->res = curl_easy_perform(cgb->curl);
+  if(cgb->res != CURLE_OK)
+    fprintf(stderr, "curl_easy_perform() failed: %s\n",
+            curl_easy_strerror(cgb->res));
   curl_formfree(formpost);
+
   return 0;
 }
+
+int CGB_authRead(CGB_t * cgb)
+{
+  FILE *fp;
+  char buf[256];
+  char *tok;
+
+  fp = fopen(auth_file,"r");
+  fread(&buf, sizeof(char), 256, fp);
+
+  tok = strtok(buf, "\n");
+  CGBString_realloc(&cgb->auth_user, strlen(tok));
+  strcpy(cgb->auth_user.mem, tok);
+
+  tok = strtok(NULL, "\n");
+  CGBString_realloc(&cgb->auth_pass, strlen(tok));
+  strcpy(cgb->auth_pass.mem, tok);
+
+  if(cgb->auth_user.size == 0 || cgb->auth_pass.size == 0) {
+    printf("couldn't parse auth\n");
+    exit(EXIT_FAILURE);
+  }
+}
+
+// }}}
 
 static int
 print_cookies(CURL *curl)
@@ -80,51 +207,13 @@ print_cookies(CURL *curl)
   return 0;
 }
 
-size_t
-WriteMemoryCallback(void *ptr, size_t size, size_t nmemb, void *data)
-{
-    size_t realsize = size * nmemb;
-    struct MemoryStruct *mem = (struct MemoryStruct *)data;
-
-    mem->memory = (char *)realloc(mem->memory, mem->size + realsize + 1);
-    if (mem->memory) {
-     memcpy(&(mem->memory[mem->size]), ptr, realsize);
-     mem->size += realsize;
-     mem->memory[mem->size] = 0;
-    }
-    return realsize;
-}
-
-int read_auth(char **user, char **pass)
-{
-  FILE *fp;
-
-  fp = fopen(auth_file,"r");
-
-  *user = *pass = NULL;
-
-  char buf[256];
-  char *tok;
-  fread(&buf, sizeof(char), 256, fp);
-  tok = strtok(buf, "\n");
-  *user = realloc(*user, strlen(tok) * sizeof(char));
-  strcpy(*user, tok);
-  tok = strtok(NULL, "\n");
-  *pass = realloc(*pass, strlen(tok) * sizeof(char));
-  strcpy(*pass, tok);
-
-  if(user == NULL || pass == NULL) {
-    printf("couldn't parse auth\n");
-    exit(EXIT_FAILURE);
-  }
-}
-
 /* Traverse the document tree */
-void dumpNode(TidyDoc doc, TidyNode body )
+void CGB_SavedQueries_parse(TidyDoc doc, TidyNode body )
 {
   TidyNode ch1, ch2;
   ctmbstr s;
   TidyAttr tattr;
+  int step=0;
 
   ch1 = tidyGetChild(body);
 
@@ -136,80 +225,94 @@ void dumpNode(TidyDoc doc, TidyNode body )
    */
 
   while(1) {
-    ch2 = tidyGetNext(ch1);
-    if(TidyTag_DIV == tidyNodeGetId(ch2)) {
-      tattr = tidyAttrGetById(ch2, TidyAttr_ID);
-      printf("id=%s\n", tidyAttrValue(tattr));
-    }
+    switch(step) {
+      case 0:
+        // Find div#footer
+        ch2 = tidyGetNext(ch1);
+        if(TidyTag_DIV == tidyNodeGetId(ch2)) {
+          tattr = tidyAttrGetById(ch2, TidyAttr_ID);
+          if(0 == strcmp("footer", tidyAttrValue(tattr))) {
+            ch1 = tidyGetChild(ch2);
+            step++;
+          }else
+            ch1 = ch2;
+        }else
+          ch1 = ch2;
+      break;
+      case 1:
+        // find ul#useful-links
 
-    ch1 = ch2;
+      break;
+
+      default:
+        fprintf(stderr, "DOM traversal failed\n");
+    }
   }
 }
 
-int main(void)
-{
-  CURL *curl;
-  CURLcode res;
-  devnull = fopen("/dev/null", "w");
-  struct MemoryStruct chunk;
-  chunk.memory=NULL; /* we expect realloc(NULL, size) to work */
-  chunk.size = 0;    /* no data at this point */
+int CGB_SavedQueries_get(CGB_t *cgb) {
+  curl_easy_setopt(cgb->curl, CURLOPT_URL, url_search_list);
 
-  char *user;
-  char *pass;
+  cgb->res = curl_easy_perform(cgb->curl);
 
-  char *cookiejar = strcat(getenv("HOME"), "/gb_cookiejar");
-
-  read_auth(&user, &pass);
-
-  curl_global_init(CURL_GLOBAL_SSL);
-
-  curl = curl_easy_init();
-  if(!curl)
-    return EXIT_FAILURE;
-
-  curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, SKIP_PEER_VERIFICATION);
-  curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, SKIP_HOSTNAME_VERIFICATION);
-
-  curl_easy_setopt(curl, CURLOPT_VERBOSE, 0);
- // curl_easy_setopt(curl, CURLOPT_COOKIEJAR, cookiejar);
-
-  gb_login(curl, user, pass);
-  curl_easy_setopt(curl, CURLOPT_URL, url_search_list);
-
-
-  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
-  curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
-
-  res = curl_easy_perform(curl);
-
-  if(res != CURLE_OK)
+  if(cgb->res != CURLE_OK)
     fprintf(stderr, "curl_easy_perform() failed: %s\n",
-            curl_easy_strerror(res));
+            curl_easy_strerror(cgb->res));
 
   TidyDoc tdoc;
-
-  //tidyOptSetBool(tdoc, TidyForceOutput, yes); /* try harder */
-  //tidyOptSetInt(tdoc, TidyWrapLen, 4096);
-  //tidySetErrorBuffer( tdoc, &tidy_errbuf );
-  //tidyBufInit(&docbuf);
 
   tdoc = tidyCreate();
   int err;
   TidyBuffer *buf;
   buf = malloc(8); // Why the hell does tidyBufInit assert buf != 0 ?
   tidyBufInit(buf);
-  tidyBufAppend(buf, (void *) chunk.memory, (uint) chunk.size);
+  tidyBufAppend(buf, (void *) cgb->response.mem, (uint) cgb->response.size);
   err = tidyParseBuffer(tdoc, buf);
   if(err < 0)
       return 1;
 
-  dumpNode( tdoc, tidyGetBody(tdoc) );
-  //fprintf(stderr, "%s\n", tidy_errbuf.bp); /* show errors */
+  CGB_SavedQueries_parse( tdoc, tidyGetBody(tdoc) );
+}
 
-  curl_easy_cleanup(curl);
-  curl_global_cleanup();
+int CGB_bz_RecordsCount_get(CGB_t *cgb, char *namedcmd, int *count) {
+  char *url=NULL;
+  int len;
+  CURLcode res;
+  len = strlen(url_namedcmd) -2 + strlen(namedcmd) +1;
+  url = realloc(&url, sizeof(char) * len);
+  snprintf(url, len, url_namedcmd, namedcmd);
 
+  curl_easy_setopt(cgb->curl, CURLOPT_URL, url);
+  cgb->res = curl_easy_perform(cgb->curl);
+  if(cgb->res != CURLE_OK)
+    fprintf(stderr, "curl_easy_perform() failed: %s\n",
+            curl_easy_strerror(cgb->res));
+    return -1;
+
+  return 0;
+}
+
+void CGB_log_response(CGB_t *cgb, char *name) {
+  if(cgb->log_response == NULL) {
+    cgb->log_response = fopen("./response.log", "a");
+  }
+
+  fprintf(cgb->log_response, "NEW %s:\n", name);
+  fwrite(cgb->response.mem, cgb->response.len-1, sizeof(char), cgb->log_response);
+  fprintf(cgb->log_response, "\n\n");
+}
+
+int main(void)
+{
+  CGB_t *cgb;
+
+  cgb = CGB_new();
+  CGB_init(cgb);
+
+  CGB_bz_login(cgb);
+  CGB_log_response(cgb, "bz_login");
+
+  CGB_cleanup(cgb);
   return EXIT_SUCCESS;
 }
 
